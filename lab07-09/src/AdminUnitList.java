d02dcedaff2fedfee52267b89b509f6fa0ee9390
Maciej Nalepa <maciejna@mops.uci.agh.edu.pl>
2019-12-03 13:46:11 +0100

    Labs 9

--- lab07-09/src/AdminUnitList.java
+++ lab07-09/src/AdminUnitList.java
@@ -1,8 +1,19 @@
+import jdk.jshell.spi.ExecutionControl;
+
 import java.io.*;
 import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class AdminUnitList {
 	List<AdminUnit> units = new ArrayList<>();
+
+	public AdminUnitList() {}
+
+	public AdminUnitList(Stream<AdminUnit> adminUnitStream) {
+		units = adminUnitStream.collect(Collectors.toList());
+	}
 
 	public void read(String filename) throws FileNotFoundException, IOException {
 		CSVReader reader = new CSVReader(filename);
@@ -54,14 +65,60 @@ public class AdminUnitList {
 		return output;
 	}
 
-	public AdminUnitList getNeighbours(AdminUnit unit, double maxDistance) {
-		throw new NotImplementedException();
+	public AdminUnitList sortInplace(Comparator<AdminUnit> cmp) {
+		units.sort(cmp);
+		return this;
+	}
+
+	public AdminUnitList sort(Comparator<AdminUnit> cmp) {
+		var clone = new AdminUnitList();
+		Collections.copy(clone.units, units);
+		return clone.sortInplace(cmp);
+	}
+
+	public AdminUnitList sortInplaceByName() {
+		Comparator<AdminUnit> cp = Comparator.comparing(a -> a.name);
+		return sortInplace(cp);
+	}
+
+	public AdminUnitList sortInplaceByArea() {
+		Comparator<AdminUnit> cp = new Comparator<AdminUnit>() {
+			@Override
+			public int compare(AdminUnit adminUnit, AdminUnit t1) {
+				return (int)(100*(adminUnit.area - t1.area));
+			}
+		};
+		return sortInplace(cp);
+	}
+
+	public AdminUnitList sortInplaceByPopulation() {
+		return sortInplace( (a,b) -> Double.compare(a.population, b.population) );
+	}
+
+	public AdminUnitList filter(Predicate<AdminUnit> pred) {
+		return new AdminUnitList(units.stream().filter(pred));
+	}
+
+	public AdminUnitList filter(Predicate<AdminUnit> pred, int limit) {
+		return new AdminUnitList(units.stream().filter(pred).limit(limit));
+	}
+
+	public AdminUnitList filter(Predicate<AdminUnit> pred, int offset, int limit) {
+		return new AdminUnitList(units.stream().filter(pred).skip(offset).limit(limit));
+	}
+
+	public AdminUnitList getNeighbours(AdminUnit unit, double maxDistance) throws ExecutionControl.NotImplementedException {
 		AdminUnitList aul = new AdminUnitList();
-		// iterate all O(n^2)
-		if (units[0].adminLevel == unit.adminLevel)
-			if (units[0].bbox.distanceTo(unit.bbox) < maxDistance || units[0].bbox.intersects(unit.bbox))
-				aul.units.add(units[0]);
+		// iterate all: O(n^2)
+		for (var i = 0; i < units.size(); i++)
+		{
+			if (units.get(0).adminLevel == unit.adminLevel)
+				if (units.get(0).bbox.distanceTo(unit.bbox) < maxDistance || units.get(0).bbox.intersects(unit.bbox))
+					aul.units.add(units.get(0));
+		}
 		return aul;
 		// R tree
+		//throw new ExecutionControl.NotImplementedException("R-tree not implemented");
+		//return aul;
 	}
 }
--- lab07-09/src/Testy.java
+++ lab07-09/src/Testy.java
@@ -5,6 +5,20 @@ public class Testy {
 	{
 		AdminUnitList ls = new AdminUnitList();
 		ls.read("admin-units.csv");
+		System.out.println("Default:");
 		ls.list(System.out, 0, 5);
+		ls.sortInplaceByName();
+		System.out.println("Sorted by name:");
+		ls.list(System.out, 0, 5);
+		System.out.println("Filtered:");
+		ls.filter(a->a.name.startsWith("K")).list(System.out, 0, 5);
+		System.out.println("Query:");
+		AdminUnitQuery query = new AdminUnitQuery()
+				.selectFrom(ls)
+				.where(a->a.area>1000)
+				.or(a->a.name.startsWith("Sz"))
+				.sort((a,b)->Double.compare(a.area,b.area))
+				.limit(100);
+		query.execute().list(System.out);
 	}
 }
